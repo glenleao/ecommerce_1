981d52d43fee0110bcb205e064611e8736f34b8c
glen leao <glenleao@gmail.com>
2019-07-25 23:20:00 -0300

    Erro page linha21

--- index.php
+++ index.php
@@ -2,17 +2,19 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Kgdzt\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
+
+	$page = new Page();
+
+	$page->setTpl("index");
     
-	$sql = new Kgdzt\DB\Sql();
-
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);	
 
 });
 
